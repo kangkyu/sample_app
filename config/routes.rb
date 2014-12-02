1571212fbb00f55d10d3bf36f58bad94625d086e
kangkyu <kangkyu1111@gmail.com>
2014-12-02 05:09:39 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
+  # get 'users/new'
+
   get "help", to: 'static_pages#help'
   get "about", to: 'static_pages#about'
   get "contact", to: 'static_pages#contact'
+  get "signup" => 'users#new'
 
   # root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
