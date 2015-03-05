f843a5bdbca690afcc7a3f53f6b7e23e323b11cc
kangkyu <kangkyu1111@gmail.com>
2015-03-05 14:10:13 -0800

    user model with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
 
   validates(:name, presence: true, length: { maximum: 50 })
-  validates :email, presence: true, length: { maximum: 255 }
+  VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VAILD_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 end
